736819a7a0c456c3af0526683826f5f88062e04a
cheyao <94928179+cheyao@users.noreply.github.com>
2024-10-12 21:06:03 +0200

    Change namespace

--- src/main/java/com/cyao/FlatMinecraft.java
+++ src/main/java/com/cyao/FlatMinecraft.java
@@ -4,8 +4,8 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class DMinecraft implements ModInitializer {
-	public static final String MOD_ID = "2d-minecraft";
+public class FlatMinecraft implements ModInitializer {
+	public static final String MOD_ID = "flat-minecraft";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
--- src/main/java/com/cyao/mixin/GameOptionsMixin.java
+++ src/main/java/com/cyao/mixin/GameOptionsMixin.java
@@ -5,16 +5,10 @@ import net.minecraft.client.option.Perspective;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(GameOptions.class)
 public class GameOptionsMixin {
-    @ModifyVariable(method = "setPerspective", at = @At("HEAD"), ordinal = 0, argsOnly = true)
-    protected Perspective thirdPerson(Perspective value) {
-        return Perspective.THIRD_PERSON_BACK;
-    }
-
     @Redirect(method = "getPerspective", at = @At(value = "FIELD", target = "Lnet/minecraft/client/option/GameOptions;perspective:Lnet/minecraft/client/option/Perspective;", opcode = Opcodes.GETFIELD))
     private Perspective getPerspective(GameOptions instance) {
         return Perspective.THIRD_PERSON_BACK;
