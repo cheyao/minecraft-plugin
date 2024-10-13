992b61015c6361826466eee814c66d453a8ff2a0
cheyao <94928179+cheyao@users.noreply.github.com>
2024-10-13 20:32:02 +0200

    3 block wide world!

--- src/main/java/com/cyao/PaperWorldGenerator.java
+++ src/main/java/com/cyao/PaperWorldGenerator.java
@@ -1,19 +1,26 @@
 package com.cyao;
 
+import com.google.common.collect.Sets;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.SharedConstants;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.registry.entry.RegistryEntry;
-import net.minecraft.world.ChunkRegion;
-import net.minecraft.world.biome.source.BiomeAccess;
+import net.minecraft.util.Util;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ChunkPos;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.world.Heightmap;
 import net.minecraft.world.biome.source.BiomeSource;
 import net.minecraft.world.chunk.Chunk;
-import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.chunk.ChunkSection;
 import net.minecraft.world.gen.StructureAccessor;
-import net.minecraft.world.gen.chunk.ChunkGeneratorSettings;
-import net.minecraft.world.gen.chunk.NoiseChunkGenerator;
+import net.minecraft.world.gen.chunk.*;
 import net.minecraft.world.gen.noise.NoiseConfig;
+
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
 
 public class PaperWorldGenerator extends NoiseChunkGenerator {
     public static final MapCodec<PaperWorldGenerator> CODEC = RecordCodecBuilder.mapCodec(
@@ -30,15 +37,115 @@ public class PaperWorldGenerator extends NoiseChunkGenerator {
     }
 
     @Override
-    public void carve(
-            ChunkRegion chunkRegion,
-            long seed,
-            NoiseConfig noiseConfig,
-            BiomeAccess biomeAccess,
-            StructureAccessor structureAccessor,
-            Chunk chunk,
-            GenerationStep.Carver carverStep
-    ) {
-        FlatMinecraft.LOGGER.info("Carving!");
+    public CompletableFuture<Chunk> populateNoise(Blender blender, NoiseConfig noiseConfig, StructureAccessor structureAccessor, Chunk chunk) {
+        GenerationShapeConfig generationShapeConfig = this.getSettings().value().generationShapeConfig().trimHeight(chunk.getHeightLimitView());
+        int i = generationShapeConfig.minimumY();
+        int j = MathHelper.floorDiv(i, generationShapeConfig.verticalCellBlockCount());
+        int k = MathHelper.floorDiv(generationShapeConfig.height(), generationShapeConfig.verticalCellBlockCount());
+        return k <= 0 ? CompletableFuture.completedFuture(chunk) : CompletableFuture.supplyAsync(Util.debugSupplier("wgen_fill_noise", () -> {
+            int l = chunk.getSectionIndex(k * generationShapeConfig.verticalCellBlockCount() - 1 + i);
+            int m = chunk.getSectionIndex(i);
+            Set<ChunkSection> set = Sets.<ChunkSection>newHashSet();
+
+            for (int n = l; n >= m; n--) {
+                ChunkSection chunkSection = chunk.getSection(n);
+                chunkSection.lock();
+                set.add(chunkSection);
+            }
+
+            Chunk var20;
+            try {
+                var20 = this.populateNoise(blender, structureAccessor, noiseConfig, chunk, j, k);
+            } finally {
+                for (ChunkSection chunkSection3 : set) {
+                    chunkSection3.unlock();
+                }
+            }
+
+            return var20;
+        }), Util.getMainWorkerExecutor());
+    }
+
+    private Chunk populateNoise(Blender blender, StructureAccessor structureAccessor, NoiseConfig noiseConfig, Chunk chunk, int minimumCellY, int cellHeight) {
+        ChunkNoiseSampler chunkNoiseSampler = chunk.getOrCreateChunkNoiseSampler(
+                chunkx -> this.createChunkNoiseSampler(chunkx, structureAccessor, blender, noiseConfig)
+        );
+        Heightmap heightmap = chunk.getHeightmap(Heightmap.Type.OCEAN_FLOOR_WG);
+        Heightmap heightmap2 = chunk.getHeightmap(Heightmap.Type.WORLD_SURFACE_WG);
+        ChunkPos chunkPos = chunk.getPos();
+        int i = chunkPos.getStartX();
+        int j = chunkPos.getStartZ();
+        AquiferSampler aquiferSampler = chunkNoiseSampler.getAquiferSampler();
+        chunkNoiseSampler.sampleStartDensity();
+        BlockPos.Mutable mutable = new BlockPos.Mutable();
+        int k = chunkNoiseSampler.getHorizontalCellBlockCount();
+        int l = chunkNoiseSampler.getVerticalCellBlockCount();
+        int m = 16 / k;
+        int n = 16 / k;
+
+        for (int o = 0; o < m; o++) {
+            chunkNoiseSampler.sampleEndDensity(o);
+
+            for (int p = 0; p < n; p++) {
+                int q = chunk.countVerticalSections() - 1;
+                ChunkSection chunkSection = chunk.getSection(q);
+
+                for (int r = cellHeight - 1; r >= 0; r--) {
+                    chunkNoiseSampler.onSampledCellCorners(r, p);
+
+                    for (int s = l - 1; s >= 0; s--) {
+                        int t = (minimumCellY + r) * l + s;
+                        int u = t & 15;
+                        int v = chunk.getSectionIndex(t);
+                        if (q != v) {
+                            q = v;
+                            chunkSection = chunk.getSection(v);
+                        }
+
+                        double d = (double)s / (double)l;
+                        chunkNoiseSampler.interpolateY(t, d);
+
+                        for (int w = 0; w < k; w++) {
+                            int x = i + o * k + w;
+                            int y = x & 15;
+                            double e = (double)w / (double)k;
+                            chunkNoiseSampler.interpolateX(x, e);
+
+                            for (int z = 0; z < k; z++) {
+                                int aa = j + p * k + z;
+                                int ab = aa & 15;
+                                double f = (double)z / (double)k;
+                                chunkNoiseSampler.interpolateZ(aa, f);
+                                BlockState blockState = chunkNoiseSampler.sampleBlockState();
+                                if (blockState == null) {
+                                    blockState = this.getSettings().value().defaultBlock();
+                                }
+
+                                if (aa != -1 && aa != 0 && aa != 1) {
+                                    blockState = AIR;
+                                    continue;
+                                }
+
+                                blockState = super.getBlockState(chunkNoiseSampler, x, t, aa, blockState);
+                                if (blockState != AIR && !SharedConstants.isOutsideGenerationArea(chunk.getPos())) {
+                                    chunkSection.setBlockState(y, u, ab, blockState, false);
+                                    heightmap.trackUpdate(y, t, ab, blockState);
+                                    heightmap2.trackUpdate(y, t, ab, blockState);
+                                    if (aquiferSampler.needsFluidTick() && !blockState.getFluidState().isEmpty()) {
+                                        mutable.set(x, t, aa);
+                                        chunk.markBlockForPostProcessing(mutable);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            chunkNoiseSampler.swapBuffers();
+        }
+
+        chunkNoiseSampler.stopInterpolation();
+        return chunk;
     }
 }
